4fa8edfdb7c618d6f7c981b1bc7ed484eb762fa9
Ulysse Vautier <ulysse.vautier@gmail.com>
2020-04-13 15:48:52 +0100

    Added enemy AI with navmesh

---
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    NavMeshAgent agent;
+
+    Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    float t = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        if(t > 0.2f)
+        {
+            t = 0f;
+            if (Vector3.Distance(target.position, transform.position) < lookRadius)
+            {
+                agent.SetDestination(target.position);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
