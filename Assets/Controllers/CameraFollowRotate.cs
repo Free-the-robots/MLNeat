a4e8391083c16a1a9cc7ca872f89f18e42bedf2c
bikbar <m.cocquempot@gmail.com>
2020-04-16 17:10:56 +0200

    [Joystick/CameraFollow/Movement] addition of asset joystick + script movementChar / CameraFollow + change of script joystick

---
+++ Assets/Controllers/CameraFollowRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowRotate : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offsetPosition;
+
+    private void LateUpdate()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+
+        transform.position = target.TransformPoint(offsetPosition);
+        transform.LookAt(target);
+
+    }
+}
---
+++ Assets/Controllers/Movement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private FloatingJoystick JoystickMoving;
+    private Vector2 mInformationFromJoystickMoving;
+
+    [SerializeField]
+    private FloatingJoystick JoystickRotate;
+    private Vector2 mInformationFromJoystickRotate;
+
+    [SerializeField]
+    private GameObject CharacterToMove;
+
+    private Vector3 mRotation;
+    private float mRotationSpeed;
+
+    private float mAngleHeadingChar;
+
+    private float mMaxSpeed = 10F;
+    private Vector3 mNormalizedDirection;
+
+    private void OnEnable()
+    {
+        JoystickRotate.OnActiveJoystick += Rotate;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRotationSpeed = 45F;
+    }
+
+    private void Update()
+    {
+        if (JoystickMoving.IsJoystickActive)
+            MoveCharacter();
+    }
+
+    private void Rotate()
+    {
+        mAngleHeadingChar = Mathf.Atan2(JoystickRotate.Direction.x, JoystickRotate.Direction.y);
+        CharacterToMove.transform.rotation = Quaternion.Euler(0F, mAngleHeadingChar * Mathf.Rad2Deg, 0F);
+    }
+
+    private void MoveCharacter()
+    {
+        mNormalizedDirection.x = JoystickMoving.Direction.x;
+        mNormalizedDirection.y = 0F;
+        mNormalizedDirection.z = JoystickMoving.Direction.y;
+
+        CharacterToMove.transform.position += mNormalizedDirection * mMaxSpeed * Time.deltaTime;
+    }
+}
